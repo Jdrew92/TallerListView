d6b4425bbbf2a094c49c91d9ff20e509290a4258
Jandres <darkkhir0@gmail.com>
2020-11-17 13:39:04 -0500

    Layouts de Areas, Volumenes y Operaciones, terminados y bilingües

--- app/src/main/java/com/example/tallerlistview/ListAreas.java
+++ app/src/main/java/com/example/tallerlistview/ListAreas.java
@@ -2,13 +2,44 @@ package com.example.tallerlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListAreas extends AppCompatActivity {
 
+    private ListView lstArea_opts;
+    private String area_opts[];
+    private ArrayAdapter<String> adapter;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_areas);
+
+        lstArea_opts = findViewById(R.id.lstAreaOpts);
+        area_opts = getResources().getStringArray(R.array.area_opts);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, area_opts);
+        lstArea_opts.setAdapter(adapter);
+
+        lstArea_opts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tallerlistview/ListVolumes.java
+++ app/src/main/java/com/example/tallerlistview/ListVolumes.java
@@ -2,13 +2,45 @@ package com.example.tallerlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListVolumes extends AppCompatActivity {
+
+    private ListView lstVol_opts;
+    private String vol_opts[];
+    private ArrayAdapter<String> adapter;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_volumes);
+
+        lstVol_opts = findViewById(R.id.lstVolOpts);
+        vol_opts = getResources().getStringArray(R.array.vol_opts);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, vol_opts);
+        lstVol_opts.setAdapter(adapter);
+
+        lstVol_opts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+        });
     }
 }
