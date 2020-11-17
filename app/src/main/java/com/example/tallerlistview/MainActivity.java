1f9bd6769e54cd7cb02a6a8e555eddd6dc1485e9
Jandres <darkkhir0@gmail.com>
2020-11-17 12:26:20 -0500

    Layout Opciones terminado y bilingüe

--- app/src/main/java/com/example/tallerlistview/MainActivity.java
+++ app/src/main/java/com/example/tallerlistview/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.tallerlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView options_list;
+    private String options[];
+    private ArrayAdapter<String> adapter;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        options_list = findViewById(R.id.lstOpts);
+        options = getResources().getStringArray(R.array.opts_array);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, options);
+        options_list.setAdapter(adapter);
+
+        options_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+        });
     }
 }
