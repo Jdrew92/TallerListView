3df58c2c71406807fb3c4360d5ed4183d2390251
Jandres <darkkhir0@gmail.com>
2020-11-18 18:46:04 -0500

    Calculo de áreas terminado sin validaciones, bilingüe

--- app/src/main/java/com/example/tallerlistview/Calculate.java
+++ app/src/main/java/com/example/tallerlistview/Calculate.java
@@ -3,6 +3,7 @@ package com.example.tallerlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.DeadObjectException;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +14,14 @@ import android.widget.TextView;
 public class Calculate extends AppCompatActivity implements View.OnClickListener{
 
     private static int strTitle, strVar1, strVar2, op;
+    private double v1, v2;
+    private String side, base, height, radius, sqr_area, rect_area, tri_area, cir_area;
+
     private TextView title, var1, var2, result;
     private EditText txtVar1, txtVar2;
     private Button btnCalc;
     private static LinearLayout grpvar2;
+    private Operations o;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,16 @@ public class Calculate extends AppCompatActivity implements View.OnClickListener
         result = findViewById(R.id.lblResult);
         grpvar2 = findViewById(R.id.grpVar2);
         btnCalc = findViewById(R.id.btnCalc);
+
+        sqr_area = getResources().getText(R.string.square_area).toString();
+        rect_area = getResources().getText(R.string.rect_area).toString();
+        tri_area = getResources().getText(R.string.tri_area).toString();
+        cir_area = getResources().getText(R.string.circle_area).toString();
+
+        side = getResources().getText(R.string.side).toString();
+        base = getResources().getText(R.string.base).toString();
+        height = getResources().getText(R.string.height).toString();
+        radius = getResources().getText(R.string.radius).toString();
 
         if (op == 0 || op == 3) {
             title.setText(strTitle);
@@ -68,16 +83,61 @@ public class Calculate extends AppCompatActivity implements View.OnClickListener
         switch (op){
             case 0:
                 Log.i("Funcion:", "Area Cuadrado");
+                o = new Operations();
+                v1 = Double.parseDouble(txtVar1.getText().toString());
+                o.setOp(sqr_area);
+                o.setData(side+": "+v1);
+                result.setText(sqr_area+": "+o.getSquareArea(v1));
                 break;
             case 1:
                 Log.i("Funcion:", "Area Rectangulo");
+                o = new Operations();
+                v1 = Double.parseDouble(txtVar1.getText().toString());
+                v2 = Double.parseDouble(txtVar2.getText().toString());
+                o.setOp(rect_area);
+                o.setData(base+ ": "+v1+" "+height+": "+v2);
+                result.setText(rect_area+": "+o.getRectangleArea(v1, v2));
                 break;
             case 2:
                 Log.i("Funcion:", "Area Triangulo");
+                o = new Operations();
+                v1 = Double.parseDouble(txtVar1.getText().toString());
+                v2 = Double.parseDouble(txtVar2.getText().toString());
+                o.setOp(tri_area);
+                o.setData(base+ ": "+v1+" "+height+": "+v2);
+                result.setText(tri_area+": "+o.getTriangleArea(v1, v2));
                 break;
             case 3:
                 Log.i("Funcion:", "Area Circulo");
+                o = new Operations();
+                v1 = Double.parseDouble(txtVar1.getText().toString());
+                o.setOp(cir_area);
+                o.setData(radius+ ": "+v1);
+                result.setText(cir_area+": "+o.getCircleArea(v1));
                 break;
         }
+    }
+
+    public void clear(View v){
+        if (op == 0 || op == 3) {
+            txtVar1.setText("");
+            result.setText("");
+            txtVar1.requestFocus();
+        }
+        if (op == 1 || op == 2){
+            txtVar1.setText("");
+            txtVar2.setText("");
+            result.setText("");
+            txtVar1.requestFocus();
+        }
+        for (int i = 0; i < Data.getOperationsList().size(); i++) {
+            Log.i("-----------", "-------------");
+            Log.i("No.", ""+i);
+            Log.i("Op", ""+Data.getOperationsList().get(i).getOp());
+            Log.i("Datos", ""+Data.getOperationsList().get(i).getData());
+            Log.i("area", ""+Data.getOperationsList().get(i).getArea());
+
+        }
+        Log.i("-----------", "-------------");
     }
 }
--- app/src/main/java/com/example/tallerlistview/Operations.java
+++ app/src/main/java/com/example/tallerlistview/Operations.java
@@ -1,4 +1,57 @@
 package com.example.tallerlistview;
 
+import java.text.DecimalFormat;
+
 public class Operations {
+    private double area;
+    private String op, data;
+    private DecimalFormat d = new DecimalFormat("0.00");
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public String getOp() {
+        return op;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public double getSquareArea(double side){
+        setArea(Double.parseDouble(d.format(side*side)));
+        Data.saveData(this);
+        return this.area;
+    }
+
+    public double getRectangleArea(double base, double height){
+        setArea(Double.parseDouble(d.format(base*height)));
+        Data.saveData(this);
+        return this.area;
+    }
+
+    public double getTriangleArea(double base, double height){
+        setArea(Double.parseDouble(d.format(0.5*base*height)));
+        Data.saveData(this);
+        return this.area;
+    }
+
+    public double getCircleArea(double radius){
+        setArea(Double.parseDouble(d.format(Math.PI*Math.pow(radius, 2))));
+        Data.saveData(this);
+        return this.area;
+    }
 }
